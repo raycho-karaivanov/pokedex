2c096bca453531fc44fa22ab9d280c61499a0f57
Raycho Karaivanov <raycho.karaivanov@gmail.com>
2015-11-08 14:29:02 +0200

    First commit

---
+++ pokedex-by-raycho/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-by-raycho
+//
+//  Created by Raycho Karaivanov on 08/11/15.
+//  Copyright © 2015 Raycho Karaivanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
